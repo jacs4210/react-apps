7478f8a903a3c36f9e2838f65ad5d70ee7d79cc3
Jairo Cuartas <jairo.cuartas18@gmail.com>
2019-08-22 11:59:46 +0000

    Creación de aplicación del clima

---
+++ weather-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WeatherLocation from './components/WeatherLocation';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        WeatherApp (Aplicación del Clima)
+        <WeatherLocation />
+    </div>
+  );
+}
+
+export default App;
