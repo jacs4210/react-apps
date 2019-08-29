e1f408d9d44ac1a423aa42c602e2c0f6791f628c
Jairo Cuartas <jairo.cuartas18@gmail.com>
2019-08-29 12:08:13 +0000

    Creación de componentes Location, WeatherData e integración con el componente padre WeatherLocation

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        WeatherApp (Aplicación del Clima)
         <WeatherLocation />
     </div>
   );
---
+++ weather-app/src/components/Location.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Location = () => (
+    <div>
+        <h1>Cali</h1>
+    </div>
+);
+
+export default Location;
