fc904719de6c9c235531e7686c0f8e9d97e9449e
Jairo Cuartas <jairo.cuartas18@gmail.com>
2019-10-08 11:39:46 +0000

    Migración del functional component WeatherLocation al Class Component. Se crea botón para el manejo del evento OnClick.

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -1,5 +1,5 @@
 /* Componente padre de Location y WeatherData */
-import React from 'react';
+import React, { Component } from 'react';
 import './styles.css';
 
 /* Componentes hijos */
@@ -15,13 +15,32 @@ const data = {
 }
 
 /* Componente funcional sin parámetros. El componente hijo Location recibe como parámetro el atributo city */
-const WeatherLocation = () => {
+/* const WeatherLocation = () => {
     return (
         <div className="weatherLocationCont">
             <Location city={"Cali"} />
             <WeatherData data={data} />
         </div>
     );
+} */
+
+/** Componente de clase sin parámetros. */
+class WeatherLocation extends Component {
+
+    handleUpdateComponent = () => {
+        console.log('Actualizado');
+    }
+
+    // Método que se encarga de renderizar la vista con este componente.
+    render() {
+        return (
+            <div className="weatherLocationCont">
+                <Location city={"Cali"} />
+                <WeatherData data={data} />
+                <button onClick={this.handleUpdateComponent}>Actualizar</button>
+            </div>
+        );
+    }
 }
 
 export default WeatherLocation;
