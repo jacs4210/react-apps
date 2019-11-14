fb63b4b32920085015879880f5cb3bec566c4c9a
Jairo Cuartas <jairo.cuartas18@gmail.com>
2019-11-14 11:24:12 +0000

    Refactorización del componente LocationList para generar la cantidad N de componentes de acuerdo a la cantidad definida de ciudades que le hayan proporcionado.

--- weather-app/src/components/LocationList.js
+++ weather-app/src/components/LocationList.js
@@ -2,16 +2,15 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import WeatherLocation from './WeatherLocation/index';
 
-const LocationList = ({ cities }) => {
-    console.log(cities);
-    return (
-        <div>
-            <WeatherLocation city="Bogota,col" />
-            <WeatherLocation city="Buenos aires,ar" />
-            <WeatherLocation city="Rio de janeiro,bra" />
-        </div>
-    );
-}
+const strToComponents = cities => (
+    cities.map(city => <WeatherLocation city={city} />)
+);
+
+const LocationList = ({ cities }) => (
+    <div>
+        {strToComponents(cities)}
+    </div>
+);
 
 LocationList.propTypes = {
     cities: PropTypes.array.isRequired
